ee90f938452c813817eb0117a66b3fcfec695ef1
Gadi Cohen <dragon@wastelands.net>
2022-10-06 11:22:45 +0100

    cursor: support sort(), limit(), skip()

--- src/cursor.ts
+++ src/cursor.ts
@@ -1,11 +1,14 @@
 import type DatabaseAdapter from "./databaseAdapter.js";
 import type Collection from "./collection.js";
-import type { Document, Filter } from "mongodb";
+import type { Document, Filter, Sort, SortDirection } from "mongodb";
 
 export default class Cursor {
   db: DatabaseAdapter;
   coll: Collection;
   filter: Filter<Document>;
+  _limit: number | null = null;
+  _sort: { sort: Sort; direction?: SortDirection } | null = null;
+  _skip: number | null = null;
 
   constructor(coll: Collection, filter: Filter<Document>) {
     this.db = coll.db;
@@ -13,17 +16,31 @@ export default class Cursor {
     this.filter = filter;
   }
 
-  // TODO
-  limit(limit: number) {
-    throw new Error("limit not implemented yet");
+  sort(sort: Sort | string, direction?: SortDirection): this {
+    this._sort = { sort, direction };
+    return this;
+  }
+
+  limit(limit: number): this {
+    this._limit = limit;
+    return this;
+  }
+
+  skip(skip: number): this {
+    this._skip = skip;
+    return this;
   }
 
   async toArray() {
     const db = await this.db.dbPromise;
-    const data = await db
-      .collection(this.coll.name)
-      .find(this.filter)
-      .toArray();
+
+    const cursor = db.collection(this.coll.name).find(this.filter);
+
+    if (this._sort) cursor.sort(this._sort.sort, this._sort.direction);
+    if (this._limit) cursor.limit(this._limit);
+    if (this._skip) cursor.skip(this._skip);
+
+    const data = await cursor.toArray();
     return data;
   }
 }
